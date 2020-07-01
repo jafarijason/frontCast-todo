5765cd576b73b64544ba601ddd1ac593a29210a2
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-07-01 17:06:06 -0400

    backend side was finished

--- backend/controllers/todo-controllers.js
+++ backend/controllers/todo-controllers.js
@@ -1,19 +1,20 @@
-let todos = [{ title: "Todo Title" }];
+const Todo = require("../models/todo");
 
-const getTodos = (req, res, next) => {
+const getTodos = async(req, res, next) => {
+    const todos = await Todo.find().exec();
     res.json(todos);
 };
 
-const createTodo = (req, res, next) => {
+const createTodo = async(req, res, next) => {
     const title = req.body.title;
 
-    const createdTodo = {
+    const createdTodo = new Todo({
         title: title,
-    };
+    });
 
-    todos.push(createdTodo);
+    await createdTodo.save();
 
-    res.json(todos);
+    res.status(201).json({ message: "Todo Created!" });
 };
 
 exports.getTodos = getTodos;
